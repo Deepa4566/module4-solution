0cd9e175d1a2081979aeb28c95d2b459bfd62612
Deepa4566 <deepa300507@gmail.com>
2025-10-02 15:21:55 +0530

    Create goodbyeSpeaker.js

---
+++ goodbyeSpeaker.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var speakWord = "Goodbye";
+  var goodbyeSpeaker = {};
+  goodbyeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.goodbyeSpeaker = goodbyeSpeaker;
+})(window);
